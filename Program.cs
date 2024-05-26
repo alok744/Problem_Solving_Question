760433f76fa14149feb69f6945c4899711fea472
Alok Sharma <56442060+alok744@users.noreply.github.com>
2024-05-26 18:38:01 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Leap_year
+{
+    class program
+    {
+      
+        public static void Main(string[] args)
+        {
+           
+            Console.Write("Enter the year : ");
+
+            int n=int.Parse(Console.ReadLine());
+            bool n1;
+            bool n2;
+
+            if (n % 4 == 0)
+            {
+                n1=true;
+                Console.WriteLine(n1);
+                Console.WriteLine("Leap Year");
+            }
+            else
+            {
+                n2 = false;
+                Console.WriteLine(n2);
+                Console.WriteLine("Not A Leap Year");
+            }
+        }
+    }
+}

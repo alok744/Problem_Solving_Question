32a4bb7738c77bd1dff9d7559df0d0cb6afc3b88
Alok Sharma <56442060+alok744@users.noreply.github.com>
2024-05-26 18:51:43 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,31 +1,25 @@
 ﻿using System;
 
-namespace Leap_year
+namespace square_number
 {
     class program
     {
-      
+
         public static void Main(string[] args)
         {
-           
-            Console.Write("Enter the year : ");
 
-            int n=int.Parse(Console.ReadLine());
-            bool n1;
-            bool n2;
+            Console.Write("Enter the Number : ");
+            int n = int.Parse(Console.ReadLine());
+            int n1;
+            for (int i = 0; i < n; i++)
+            {
+                if(i<=n)
+                {
+                    n1=i*i;
+                    Console.WriteLine(n1);
+                }
+            }
 
-            if (n % 4 == 0)
-            {
-                n1=true;
-                Console.WriteLine(n1);
-                Console.WriteLine("Leap Year");
-            }
-            else
-            {
-                n2 = false;
-                Console.WriteLine(n2);
-                Console.WriteLine("Not A Leap Year");
-            }
         }
     }
 }
